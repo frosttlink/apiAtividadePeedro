aedeaab4fadb26536e39121da2d5ee966a7b73db
Leonardo Alves <leolorena.alves@gmail.com>
2024-11-18 12:00:02 -0300

    a

--- src/controller/chamadoController.js
+++ src/controller/chamadoController.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import cors from 'cors';
 import addChamadoService from "../service/chamado/adicionarChamadoService.js";
 import deletarChamadoService from "../service/chamado/deletarChamadoService.js";
 import consultarChamadoPorIdService from "../service/chamado/consultarChamadoPorIdService.js";
@@ -7,66 +8,55 @@ import consultarChamadoService from "../service/chamado/ConsultarChamadoService.
 import alterarChamadoService from "../service/chamado/alterarChamadoService.js";
 
 const endpoint = Router();
-
-
+endpoint.use(cors());
 endpoint.get('/chamado', async (req, resp) => {
     try {
-        let registros = await consultarChamadoService()
+        let registros = await consultarChamadoService();
         resp.status(200).send(registros);
     } catch (err) {
-        resp.status(400).send({error: err.message});
-    }
-})
-
-endpoint.get('/chamado/:id', async (req, resp) => {
-    try {
-        let id = req.params.id
-        let registros = await consultarChamadoPorIdService(id)
-        resp.status(200).send(registros);
-    } catch (err) {
-        resp.status(400).send({error: err.message});
-    }
-})
-
-
-endpoint.post('/chamado', async (req,resp) => {
-    try {
-        let chamado = req.body
-
-        let idGerado = await addChamadoService(chamado);
-
-        resp.send({
-            id: idGerado
-        })
-    } catch (err) {
-        resp.status(400).send({error:err.message});
-    }
-})
-
-endpoint.put ('/chamado/:id', async (req, resp) => {
-    try {
-        let chamado = req.body
-        let id = req.params.id
-        await alterarChamadoService(chamado, id)
-        resp.status(200).send();
-    } catch (err) {
-        resp.status(400).send({error: err.message });
+        resp.status(400).send({ error: err.message });
     }
 });
 
+endpoint.get('/chamado/:id', async (req, resp) => {
+    try {
+        let id = req.params.id;
+        let registros = await consultarChamadoPorIdService(id);
+        resp.status(200).send(registros);
+    } catch (err) {
+        resp.status(400).send({ error: err.message });
+    }
+});
+
+endpoint.post('/chamado', async (req, resp) => {
+    try {
+        let chamado = req.body;
+        let idGerado = await addChamadoService(chamado);
+        resp.status(201).send({ id: idGerado });
+    } catch (err) {
+        resp.status(400).send({ error: err.message });
+    }
+});
+
+endpoint.put('/chamado/:id', async (req, resp) => {
+    try {
+        let chamado = req.body;
+        let id = req.params.id;
+        await alterarChamadoService(chamado, id);
+        resp.status(200).send({ message: 'Chamado atualizado com sucesso' });
+    } catch (err) {
+        resp.status(400).send({ error: err.message });
+    }
+});
 
 endpoint.delete('/chamado/:id', async (req, resp) => {
     try {
         let id = req.params.id;
-        await deletarChamadoService(id)
-        resp.status(200).send()
-        
-    } catch (ere) {
-        resp.status(400).send({error: err.message });
+        await deletarChamadoService(id);
+        resp.status(200).send({ message: 'Chamado deletado com sucesso' });
+    } catch (err) {
+        resp.status(400).send({ error: err.message });
     }
-})
-
-
-
+});
 
 export default endpoint;
